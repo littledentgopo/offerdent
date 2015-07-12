62ebce08e930e034756666a23758bbbade83f19e
littlemangopo <littlemangopo@gmail.com>
2015-07-12 16:10:48 +0300

    Improving speed

---
+++ js/main.js
@@ -0,0 +1,14 @@
+---
+---
+
+    {% include js/jquery-1.11.0.js %}
+
+    {% include js/bootstrap.min.js %}
+
+    {% include js/jquery.easing.min.js %}
+    
+    {% include js/classie.js %}
+
+    {% include js/cbpAnimatedHeader.js %}
+
+    {% include js/freelancer.js %}
